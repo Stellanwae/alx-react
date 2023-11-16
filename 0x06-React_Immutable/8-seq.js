23f7e03ca702834229df3c900102e1c4282eeca1
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-11-16 20:21:23 +0300

    push react elements

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,26 @@
+import { Seq } from 'immutable';
+
+export default function printBestStudents(object) {
+  const seq = Seq(object);
+
+  const filtered = seq.filter((student) => {
+    student.firstName.charAt(0).toUpperCase();
+
+
+    return student.score > 70;
+  });
+
+  function capitFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const JSObject = filtered.toJS();
+
+  Object.keys(JSObject).map((key) => {
+    JSObject[key].firstName = capitFirstLetter(JSObject[key].firstName);
+    JSObject[key].lastName = capitFirstLetter(JSObject[key].lastName);
+    return JSObject[key];
+  });
+
+  console.log(JSObject);
+}
